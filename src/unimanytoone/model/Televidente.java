fbf9b8572d98097553fbd272cbad782785997167
Eduardo Jimenez <worldjiz@gmail.com>
2015-07-10 18:51:48 -0500

    Agregando archivos de configuracion

--- src/unimanytoone/model/Televidente.java
+++ src/unimanytoone/model/Televidente.java
@@ -1,5 +1,41 @@
 package unimanytoone.model;
 
 public class Televidente {
+    private Integer idTelevidente;
+    private String nombre;
+    private Televisiva televisiva;
+
+    public Televidente() {
+    }
+
+    public Televidente(String nombre, Televisiva televisiva) {
+        this.nombre = nombre;
+        this.televisiva = televisiva;
+    }
+
+    public Integer getIdTelevidente() {
+        return idTelevidente;
+    }
+
+    public void setIdTelevidente(Integer idTelevidente) {
+        this.idTelevidente = idTelevidente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Televisiva getTelevisiva() {
+        return televisiva;
+    }
+
+    public void setTelevisiva(Televisiva televisiva) {
+        this.televisiva = televisiva;
+    }
+    
     
 }
